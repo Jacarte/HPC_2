851a419e7252bc772e5ed4bd9e59219ac0fba2f6
Jacarte <javierca@kth.se>
2020-09-21 19:28:38 +0200

    First approach to calculate positions and forces

--- nbody/nbody.c
+++ nbody/nbody.c
@@ -1,35 +1,43 @@
 
 
 #include <stdio.h>
+#include <math.h>
 
+#define DIM 2
+typedef double vect_t[DIM];
 
-int time_delta;
+#define G 6.673e-11
+
+double time_delta;
 int n;
-int *initial_position;
-int *initial_velocity;
+vect_t *position;
+vect_t *velocity;
+vect_t *forces;
+double *masses;
 int T;
 
 int main(void){
 
 	printf("Set input data as follows: <number of particles> <delta_time> <Time> <for each particle <mass> <x0> <y0> <vx0> <vy0>>\n");
-	scanf("%d %d %d", &n, &time_delta, &T);
+	scanf("%d %lf %d", &n, &time_delta, &T);
 
-	initial_position = (int*) malloc(sizeof(int)*n*2);
-	initial_velocity = (int*) malloc(sizeof(int)*n*2);
+	position = (vect_t*) malloc(sizeof(vect_t)*n);
+	velocity = (vect_t*) malloc(sizeof(vect_t)*n);
+	forces = (vect_t*) malloc(sizeof(vect_t)*n);
+	masses = (double*) malloc(sizeof(double)*n);
 
 	for(int i = 0; i < n; i++){
-		// Components should be stored one at a side, it would be better to load
-		scanf("%d %d %d %d", &initial_position[i], &initial_position[i + 1], &initial_velocity[i], &initial_velocity[i + 1]); 
+		scanf("%lf %lf %lf %lf %lf", &masses[i], &position[i][0], &position[i][1], &velocity[i][0], &velocity[i][1]); 
 	}
 
 	#ifdef DEBUG
 
 	for(int i = 0; i < n; i++){
-		printf("Positions\n\t%d %d\n", initial_position[i], initial_position[i + 1]);
+		printf("Positions\n\t%lf %lf\n", position[i][0], position[i][1]);
 	}
 
 	for(int i = 0; i < n; i++){
-		printf("Velocities\n\t%d %d\n", initial_velocity[i], initial_velocity[i + 1]);
+		printf("Velocities\n\t%lf %lf\n", velocity[i][0], velocity[i][1]);
 	}
 
 	#endif
@@ -41,18 +49,36 @@ int main(void){
 	for(int i = 1; i <= T; i++){
 		int time_step = i*time_delta;
 
+
+
+		for(int q = 0; q < n; q++){
+			for(int k = 0; k < n; k++){
+				if(k != q){
+					double xdiff = position[q][0] - position[k][0];
+					double ydiff = position[q][1] - position[k][1];
+
+					double dist = sqrt(xdiff*xdiff + ydiff*ydiff); 
+            		double dist_cubed = dist*dist*dist; 
+
+					forces[q][0] -= G*masses[q]*masses[k]/dist_cubed * xdiff; 
+					forces[q][1] -= G*masses[q]*masses[k]/dist_cubed * ydiff; 
+				}
+			}
+		}
+
 		if(i == T){
 			printf("Simulation results\n idx - (postion) (velocity)\n");
 
 			for(int i = 0; i < n; i++){
-				printf("%d - (%d, %d)  (%d, %d)", i, initial_position[i], initial_position[i + 1], initial_velocity[i], initial_velocity[i + 1]);
+				printf("%d - (%lf, %lf)  (%lf, %lf)", i, position[i][0], position[i][1], velocity[i][0], velocity[i][1]);
 			}
 			printf("\n");
 		}
 	}
 
-	free(initial_position);
-	free(initial_velocity);
-
+	free(position);
+	free(velocity);
+	free(forces);
+	free(masses);
 	return 0;
 }
