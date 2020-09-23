4f9bc0d2ca9e14a5bf7adb5d15c6a42d23092eb8
Jacarte <javierca@kth.se>
2020-09-23 13:34:43 +0200

    Measuring both serial and REDUCED

--- nbody/run_all.sh
+++ nbody/run_all.sh
@@ -2,10 +2,20 @@
 
 N="500 1000 2000"
 
-# SERIAL
+echo "SERIAL"
 for i in $N
 do
 	export CFLAGS=-DMEASURE_TIME
+	make clean 1>/dev/null 2> /dev/null
+	make 1>/dev/null 2> /dev/null
+	
+	./nbody.out <<< "$i 0.05 100"
+done
+
+echo "REDUCED"
+for i in $N
+do
+	export CFLAGS="-DMEASURE_TIME -DREDUCED"
 	make clean 1>/dev/null 2> /dev/null
 	make 1>/dev/null 2> /dev/null
 	
